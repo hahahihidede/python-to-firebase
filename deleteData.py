2644cc7f1f5dec5dc1529374458529c4ba166f71
Dede Rohmat H <54473030+hahahihidede@users.noreply.github.com>
2020-12-31 09:42:52 +0700

    Update deleteData.py

--- deleteData.py
+++ deleteData.py
@@ -1,1 +1,10 @@
+from firebase import firebase
 
+#declare your database url and make sure to enable write and read setting in your firebase database sett
+firebase = firebase.FirebaseApplication('YOUR DATABASE URL', None)
+
+
+#spesific table of your database (e.g '/-MPmcn2rs3o2xxxqlAY/') and variable you want to delete (e.g 4)
+result = firebase.put('SPECIFIED TABLE OF YOUR DATABASE',4)
+print('Deleted')
+print(result)
